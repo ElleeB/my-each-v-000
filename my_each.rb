df67b8d5b41ee7b23673f373a91ef19628b67739
Laura Trager <laura.trager@gmail.com>
2017-10-30 15:41:47 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+
+def my_each(collection)
+  index = 0
+
+  while index < collection.length
+    yield collection[index]
+    index += 1
+  end
+  
+end
+
+my_each(collection) do |index|
+  puts index
 end
